ab60b4ad69b654da42341a168b4a16c4c3433b41
Jairus Mark <jairuscollantes0@gmail.com>
2026-03-25 23:44:00 +0800

    lmao

---
+++ part2.py
@@ -0,0 +1,27 @@
+class GameDB:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def add_character(self):
+        pass
+
+    def add_item(self):
+        pass
+
+    def view_characters(self):
+        pass
+
+    def view_items(self):
+        pass
+
+    def update_character(self):
+        pass
+
+    def delete_item(self):
+        pass
+
+    def search_item(self):
+        pass
