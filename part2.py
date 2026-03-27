59ec6994e3741b059ed6b7e99b7049eff9d0badd
Jairus Mark <jairuscollantes0@gmail.com>
2026-03-27 21:31:31 +0800

    wewas

--- part2.py
+++ part2.py
@@ -206,31 +206,3 @@ def main():
 
 
 main()
-
-class GameDB:
-    def __init__(self):
-        pass
-
-    def connect(self):
-        pass
-
-    def add_character(self):
-        pass
-
-    def add_item(self):
-        pass
-
-    def view_characters(self):
-        pass
-
-    def view_items(self):
-        pass
-
-    def update_character(self):
-        pass
-
-    def delete_item(self):
-        pass
-
-    def search_item(self):
-        pass
